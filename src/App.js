db461eb42e21b566c6a194f6e9d51fcdb8d5a0ed
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-01-31 20:34:30 -0700

    added menu

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-	  
-
-
 
 	  <div>	
 	  <div>
@@ -23,7 +20,7 @@ function App() {
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#features">Git Commands</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
     <Form inline>
