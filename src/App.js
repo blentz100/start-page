9c4ef0c8a73d1b8d9796065c8cb0784f52a8e30c
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-01-31 20:33:12 -0700

    added menue

--- src/App.js
+++ src/App.js
@@ -4,14 +4,42 @@ import React  from 'react';
 import Clock from 'react-live-clock';
 import Card from 'react-bootstrap/Card'
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 //import beach from '../src/images/beach.jpg'
 
 function App() {
   return (
+	  
+
+
+
+	  <div>	
+	  <div>
+ <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
+	  </div>
+
+
+
+
     <div className="App" style={{display: 'flex', flexDirection: 'row'}}>
 		<Card style={{ width: '8rem' }}> 
 			<Card.Body> 
-				<Card.Title className="card-title">Pacific1</Card.Title>
+				<Card.Title className="card-title">Pacific</Card.Title>
 				<Card.Text>
 					<Clock format={'hh:mm:ss:a'} ticking={true} timezone={'US/Pacific'} />
 				</Card.Text>
@@ -30,7 +58,10 @@ function App() {
 				</Card.Text>
 			</Card.Body>
 		  <ListGroup className="list-group-flush">
+			<ListGroupItem>CEI </ListGroupItem>
 			<ListGroupItem>MCCCD </ListGroupItem>
+			<ListGroupItem>Missoula </ListGroupItem>
+			<ListGroupItem>Idaho State </ListGroupItem>
 			<ListGroupItem>Arapahoe</ListGroupItem>
 		  </ListGroup>
 		</Card>
@@ -53,11 +84,13 @@ function App() {
 				</Card.Text>
 			</Card.Body>
 		  <ListGroup className="list-group-flush">
-			<ListGroupItem>CCWA Virginia</ListGroupItem>
+			<ListGroupItem>CCWA </ListGroupItem>
 			<ListGroupItem>Nashua</ListGroupItem>
 		  </ListGroup>
 		</Card>
     </div>
+
+	  </div>
   );
 }
 
